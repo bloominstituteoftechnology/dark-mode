98785ae4b1dc0f2a12f51b1732e5f6c59b9fe042
Elise Spain <elisespain@gmail.com>
2019-07-16 14:18:44 -0700

    Setup dark mode function

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import React, { useState} from 'react';
+
+
+
+const useLocalStorage = (key, initialValue) => {
+
+  const [storedValue, setStoredValue] = useState(()=> {
+      const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });  
+
+  const setValue = value =>{
+      localStorage.setItem(key, JSON.stringify(value));
+      setStoredValue(value);
+  }
+
+    return[storedValue, setValue];
+};
