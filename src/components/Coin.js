7c218221fd1c32083f4c7c98b3810229a169c1f8
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-16 19:15:04 -0600

    Added coinc images

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import "../styles.css";
+
 const Coin = ({ specificCoinData }) => {
   let descriptionObj = { ...specificCoinData.description };
   let communityObj = { ...specificCoinData.community_data };
+  let imageObj = { ...specificCoinData.image };
 
   let description = Object.keys(descriptionObj).map(el => {
     if (el === "en") {
@@ -14,19 +17,31 @@ const Coin = ({ specificCoinData }) => {
     return (
       <div>
         <h4>
-          {el} : {communityObj[el]}
+          {el[0].toUpperCase() +
+            el
+              .slice(1)
+              .split("_")
+              .join(" ")}{" "}
+          : {communityObj[el]}
         </h4>
       </div>
     );
   });
 
+  let image = Object.keys(imageObj).map(el => {
+    if (el === "thumb") {
+      return imageObj[el];
+    }
+  });
+
   return (
-    <div>
+    <div className="coin-container">
       <h1>{specificCoinData.name}</h1>
       <h2>CoinGecko Rank: {specificCoinData.coingecko_rank}</h2>
+      <img src={image} />
 
       <p>{description}</p>
-      <h3>Tickers:</h3>
+      <h3>{specificCoinData.name} Community Data:</h3>
       <h4>{community}</h4>
       {/* {specificCoinData.tickers.map(ticker => (
         <div>
