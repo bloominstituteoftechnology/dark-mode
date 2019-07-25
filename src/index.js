c1a43ed054e0a6049c424acf9a4b903db63e45af
ch04937 <97hernandez.c@gmail.com>
2019-07-25 13:03:29 -0700

    project set up complete

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
+import './index.css';
+import App from './App';
 
-import "./styles.scss";
+import { BrowserRouter as Router } from 'react-router-dom'
 
-const App = () => {
-  const [coinData, setCoinData] = useState([]);
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root'));
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
-  return (
-    <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
-    </div>
-  );
-};
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
