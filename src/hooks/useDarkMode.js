a93050443828810cdb523822d52c6b81ef3aa273
dangolbeeker <scottb305@outlook.com>
2019-07-25 18:11:24 -0400

    useDarkMode Hook Built

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,4 +3,13 @@ import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = () => {
     const [storedValue, setValue] = useLocalStorage("darkMode");
-}
+
+    useEffect (() => {
+        let body = document.querySelector("body");
+        storedValue
+        ? body.classList.add("dark-mode")
+        : body.classList.remove("dark-mode");
+    }, [storedValue]);
+
+    return [storedValue, setValue];
+};
