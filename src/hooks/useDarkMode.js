95d05d211b50ce9c1edcf2f118465658c08273a5
ebisLab <ebaiden@techie.com>
2019-07-26 07:22:07 -0400

    dark mode set

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,16 @@
 import {useEffect} from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const useDarkMode = () =>{
+const useDarkMode = (initVal) =>{
+const [darkMode, setDarkMode] = useLocalStorage('dark-mode', initVal);
+useEffect(()=> {
+if (darkMode) {
+document.body.classList.add('dark-mode')
+}else {
+    document.body.classList.remove('dark-mode')
+}}, [darkMode]);
 
+return [darkMode, setDarkMode];
 };
 
 export default useDarkMode;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [local, setLocal] = useLocaStorage('testKey', "something else")
+  const [local, setLocal] = useLocaStorage('testKey')
   
 
   useEffect(() => {
