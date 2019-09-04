272215347df1980dd8860688e4b9511ad15492af
zachary-peasley <zpman95@hotmail.com>
2019-09-04 15:20:40 -0500

    updated LocalStorage

---
+++ src/hooks.js/useLocalStorage.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+const localStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+
+    return (item ? JSON.parse(item) : initialValue)
+});
+    const setValue = value => {
+        setStoredValue(value);
+        localStorage.setItem(key, JSON.stringify(value));
+
+    };
+    return [setValue, storedValue];
+};
+
+export default localStorage;
