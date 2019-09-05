d023c1be8efad6b9ab5f1e0f7d1ba91ba0124048
Julie <julieantonio38@gmail.com>
2019-09-04 20:04:13 -0400

    step one complete

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import useLocalStorage from './useLocalStorage'
+
+export const useDarkMode = () => {
+    
+}
---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      });
+
+    const setValue = (value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+        setStoredValue(value);
+    };
+
+    return [storedValue, setValue]
+}
