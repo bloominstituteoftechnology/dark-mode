7ed1f69be93b130f6809c5342705b542da7760e7
AceIsHuman <menoman1995@yahoo.com>
2019-09-10 21:44:14 -0500

    create useEffect to add className to body tag

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode =  () => {
   const [darkMode, setDarkMode] = useLocalStorage('darkMode', false);
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    darkMode ? body.classList.add('dark-mode') : body.classList.remove('dark-mode');
+  }, [darkMode])
 }
