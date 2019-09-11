7186a18e818d2382a9b2b8be1721c027db769a4e
AceIsHuman <menoman1995@yahoo.com>
2019-09-10 21:15:59 -0500

    create storedValue hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  })
+}
