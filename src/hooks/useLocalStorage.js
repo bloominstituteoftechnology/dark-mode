d34a8b8f72f49ba620126b1ff1f6e77b7f2c31fc
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-02 17:02:33 -0400

    initial commit

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    
+    const [storedValue, setStoredValue] = useState(() => {
+    
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+
+    });
+    return [storedValue];
+};
