37b0c30c526e49a6641c135c7eacda2d4b8aa02d
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-02 13:00:47 +0100

    api link uncommented

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ const App = () => {
   useEffect(() => {
     axios
       .get(
-        //"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
