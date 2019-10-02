b52d6d092141cccbb507e1fde004d0845d2d4136
dAVE Inden <davidinden@gmail.com>
2019-10-02 13:52:04 -0700

    dark mode toggle functions and stores to localStorage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -6,8 +6,10 @@ export const useDarkMode = (initialValue) => {
 
     useEffect(() => {
         if (darkMode) {
+            document.body.classList.add('dark-mode');
             console.log('dark mode enabled!')
         } else {
+            document.body.classList.remove('dark-mode');
             console.log('dark mode disabled!');
         }
     }, [darkMode])
