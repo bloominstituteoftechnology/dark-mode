7470f8cbda8b34e03c94bb263f2d77d0e4f08e5a
Dom <domeccleston@gmail.com>
2019-10-02 15:55:46 +0100

    added in routing stretch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDarkMode } from './hooks';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-/*   const [darkMode, setDarkMode] = useState(false); */
 
-  const [darkModeNew, setDarkModeNew] = useDarkMode(false)
+  const [darkModeNew, setDarkModeNew] = useDarkMode(true)
+  console.log(darkModeNew)
 
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/">Home</NavLink>
       <div className="dark-mode__toggle">
-        <div
+       <div
           onClick={setDarkModeNew}
           className={darkModeNew ? 'toggle toggled' : 'toggle'}
         />
--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import axios from "axios";
-
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import About from './components/About';
+import Home from './components/Home';
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
-  }, []);
+  
   return (
-    <div className="App">
-      <Navbar />
-      <Charts coinData={coinData} />
-    </div>
+    <>
+    <Route exact path="/" component={Home} />
+    <Route path ="/about" component={About} />
+    </>
   );
 };
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Router><App /></Router>, rootElement);
