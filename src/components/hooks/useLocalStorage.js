77d6009a6cd2f4e42f6d4059c132cf0de168c17d
“marzier” <atton.rand.23@gmail.com>
2019-10-02 12:56:49 -0700

    mvp

---
+++ src/components/hooks/useLocalStorage.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
+};
