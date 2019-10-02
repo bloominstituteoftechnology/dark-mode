eada76d6abe481cb0d21ef5c9554f8d133f08969
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-02 12:43:53 +0100

    hook built to toggle class name

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ Follow these steps for completing your project.
 ## Minimum Viable Product
 
 - [ ] Build a custom hook that let's you save data to localStorage
-- [ ] Build a second custom hook that sets the `dark-mode` class on the body element
+- [x] Build a second custom hook that sets the `dark-mode` class on the body element
 - [ ] Compose your two new hooks together to be able to set and persist your user's dark mode preference in your app
 <!--
 - [ ] Write tests that use mocks and spies to handle dependencies in your components
--- src/index.js
+++ src/index.js
@@ -7,20 +7,37 @@ import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
+const SetViewMode = (initialValue) => {
+  const [darkMode, setDarkMode] = useState(initialValue);
+
+  const toggleMode = (e) => {
+    e.preventDefault();
+
+    if(darkMode) {
+      setDarkMode(false);
+    }else {
+      setDarkMode(true);
+    }
+  };
+
+  return [darkMode, toggleMode];
+}
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [mode, setMode] = SetViewMode(false);
 
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+        //"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
   return (
-    <div className="App">
-      <Navbar />
+    <div className={mode ? 'App dark-mode' : 'App'}>
+      <Navbar mode={mode} setMode={setMode}/>
       <Charts coinData={coinData} />
     </div>
   );
