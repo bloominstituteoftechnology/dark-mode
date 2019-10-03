3deb71edd559872fef77e616af7774f371c5d005
kiyani <bamba.kiyani@gmail.com>
2019-10-03 00:47:04 -0400

    created useDarkMode file

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useLocalStorage = (key, initialValue) => {
+export default const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);     
      //Check if the window.localStorage has a specific item (retrieved by key) in it, Return that item from local storage if it exists or the initialValue otherwise
@@ -13,3 +13,4 @@ const useLocalStorage = (key, initialValue) => {
   };
   return [storedValue, setValue];
 };
+
