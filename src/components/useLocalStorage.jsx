0a1e5318950b4a9db641bbf7a33b6f977f4de1c6
Ricardo Barbosa <ric3639@gmail.com>
2019-11-05 20:34:45 -0800

    setting up localStorage with state

---
+++ src/components/useLocalStorage.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+//"key" should have a calue of "dark-mode"
+//"initialValue" should have a value of boolean false
+
+const useLocalStorage = (key, initialValue) => {
+    const [darkMode, setDarkMode] = useState(() => {
+
+    })
+}
