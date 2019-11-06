21da6484d74c446275eaba6bb126260d6ae4c18f
Scott Grobe <smgrobe@Scotts-MacBook-Pro.local>
2019-11-06 17:40:32 -0500

    add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+    return [storedValue];
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+}
