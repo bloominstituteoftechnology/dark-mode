e0037d8614f4bc65cf1b8cff97f6061cd5b3f824
Veto Ramirez <vetorobert3@gmail.com>
2019-11-07 19:42:56 -0800

    created useDarkMode component. Trying to make it work

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,11 @@
-// import { useState } from 'react';
+import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage.js';
+
+useEffect(() => {
+      if (storedValue === true) {
+        return document.body = dark-mode;
+      }
+}, [])
 
 export const useDarkMode = (key, initialValue) => {
   const [value, setValue] = useLocalStorage(key, initialValue);
