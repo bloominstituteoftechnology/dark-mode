fcb3ec9cb4eddeb9af2a59b0324330a58cb20daa
Scott Grobe <smgrobe@Scotts-MacBook-Pro.local>
2019-11-08 13:15:55 -0500

    ternanry & if/else swap for dark mode class

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,11 +4,15 @@ import useLocalStorage from './useLocalStorage';
 const useDarkMode = (key) => {
     const [values, setValues] = useLocalStorage('darkMode');
     useEffect(() => {
-        if (values === true) {
-            document.body.classList.add('dark-mode')
-        } else {
-            document.body.classList.remove('dark-mode')
-        }
+        
+        // if (values === true) {
+        //     document.body.classList.add('dark-mode')
+        // } else {
+        //     document.body.classList.remove('dark-mode')
+        // }
+
+        values ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+
     }, [values]);
 
     return [values, setValues];
