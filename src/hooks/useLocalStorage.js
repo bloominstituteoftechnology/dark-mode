12c619be4929ebe67dcd0743b0370dc602c95619
Veto Ramirez <vetorobert3@gmail.com>
2019-11-07 16:31:26 -0800

    Wrote the state for the useLocalStorage component.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,3 +1,8 @@
+import React, { useState } from 'react';
+
 const useLocalStorage = (key, initialValue) => {
-  
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
 }
