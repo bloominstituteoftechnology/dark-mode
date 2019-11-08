8e0a6c48039d562e8e8d7a8b5b480a34fdfc3f01
Veto Ramirez <vetorobert3@gmail.com>
2019-11-07 20:05:11 -0800

    Imported the darkmode component to the navbar component. Trying to make it work.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage.js';
 
-useEffect(() => {
-      if (storedValue === true) {
-        return document.body = dark-mode;
-      }
+export const useDarkMode = (key, initialValue) => {
+
+  useEffect(() => {
+    if (storedValue === true) {
+      return document.body = dark-mode;
+    } else {
+      return document.body = app;
+    }
 }, [])
 
-export const useDarkMode = (key, initialValue) => {
   const [value, setValue] = useLocalStorage(key, initialValue);
   const handleChanges = updatedValue => {
     setValue(updatedValue);
