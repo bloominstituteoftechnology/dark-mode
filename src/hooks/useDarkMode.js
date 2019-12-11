bc14d8d1df856645e1051d8b335553efe785e68d
Trey McGarity <trey.mcgarity99@gmail.com>
2019-12-10 18:56:32 -0600

    added hooks setup useLocalStorage

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage(key);
+
+    useEffect(() => {
+
+    })
+}
