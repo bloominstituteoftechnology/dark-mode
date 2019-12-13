0a95d5c75ec6e0995bbe054bb7a53f49bedc3153
Ariel Rodriguez <Ariels713@gmail.com>
2019-12-13 17:26:54 +0000

    working app

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+};
