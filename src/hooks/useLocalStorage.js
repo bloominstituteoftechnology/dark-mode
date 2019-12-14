9a4f76eb6bdd84459ca88d6c41edee790cf15d41
wtrawlings <rawlings.warren@gmail.com>
2019-12-14 13:19:15 -0500

    starting custom hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,1 @@
+//this is a thing I created myself not a default hook
--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ const App = () => {
   const [coinData, setCoinData] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
+    axios.get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then(res => setCoinData(res.data))
