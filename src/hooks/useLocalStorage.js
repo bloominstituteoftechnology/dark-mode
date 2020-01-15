c372c77bb80fb446b4c78d94cdb02051dbb38d4f
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-15 13:22:33 -0800

    setValue established

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,16 @@
 import {useState} from 'react'
 
 export const useLocalStorage =  (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(()=>{
+        const item = window.localStorage.getItem(key);
 
-}
+        return item ? JSON.parse(item) : initialValue;
+    })
+    return [storedValue];
+};
 
+const setValue = value => {
+    setStoredValue(value);
+
+    window.localStorage.setItem(key, JSON.stringify(value));
+};
