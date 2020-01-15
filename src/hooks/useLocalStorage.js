1e829dae1c222ec2fe35464d276cbb8943f65162
nicbongo <nicbongo@protonmail.com>
2020-01-15 16:39:31 -0400

    step one complete

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => { 
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue
+  });
+
+  const setValue = newVal => {
+    setStoredValue(newVal)
+    window.localStorage.setItem(key, JSON.stringify(newVal))
+  }
+
+  return [storedValue];
+
+};
+
