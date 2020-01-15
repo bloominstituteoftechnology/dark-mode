1e291e4d52292d7a3148d2ccba373c6db4a04ab5
Milo Rastgoo <hello@informagician.com>
2020-01-15 15:51:15 -0500

    localstroage setup - initial commit

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export const useLocalStorage = (key, initialValue) => {
+    const [ storedValue, setStoredValue ] = useState(() => {
+        if (localStorage.getitem(key)) {
+            return JSON.parse(localStorage.getItem(key));
+        } else {
+            localStorage.setItem(key, JSON.stringify(initialValue));
+            return initialValue;
+        }
+    })
+
+    const setValue = newValue => {
+        setStoredValue(newValue);
+        localStorage.setItem(key, JSON.stringify(newValue));
+    }
+
+    return [storedValue, setValue];
+
+}
