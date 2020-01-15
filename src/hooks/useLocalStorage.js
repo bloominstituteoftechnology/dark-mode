26387444e260ba72b187f1a6f9a00cd0201146df
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-15 14:14:53 -0800

    mvp met

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -11,6 +11,7 @@ export const useLocalStorage = (key, initialValue) => {
   const setValue = value => {
     setStoredValue(value);  
     window.localStorage.setItem(key, JSON.stringify(value));
+    
   };
 
   return [storedValue, setValue];
