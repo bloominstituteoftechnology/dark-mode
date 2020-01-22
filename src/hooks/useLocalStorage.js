de72b6621f3d1970e30b01a547dc03220c739197
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-21 21:19:34 -0800

    Step 3 completed.Used the costum hook in the nav bar component

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,11 +5,11 @@ export const useLocalStorage = (key, initialValue)=>{
         const item = window.localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
   });
-  
-  setValue = value => {
+
+  const setValue = value => {
     setStoredValue(value);
     window.localStorage.setItem(key, JSON.stringify(value));
 };
-  return [storedValue];
+  return [storedValue, setStoredValue, setValue];
 }
    
