34e349283c1aa4e950d5de730db3cd186b3c2c50
Veto Ramirez <vetorobert3@gmail.com>
2020-01-23 20:05:06 -0800

    Finished the dark mode toggle.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,13 +3,18 @@ import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useLocalStorage(false);
+  const [darkMode, setDarkMode] = useLocalStorage('isDark', false);
 
   useEffect(() => {
-    if (useLocalStorage === true) {
-      
+    if (darkMode === true) {
+      document.body.classList.add('dark-mode');
+      console.log('this!')
+    } else {
+      document.body.classList.remove('dark-mode');
     }
-  }, [])
+  }, [darkMode])
+
+  return [darkMode, setDarkMode];
 } 
 
 export default useDarkMode;
