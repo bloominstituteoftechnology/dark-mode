e7a205a36b36d53df704aa1b87db8a9b170882e2
Antoinette <create@aplusmedia.info>
2020-01-29 12:55:49 -0500

    trying to make it work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import useLocalStorge from "../hooks/useLocalStorage";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useLocalStorge(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,19 @@
+import {useLocalStorage} from "../hooks/useLocalStorage";
+import {useEffects} from "react";
+
+const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage('DarkModeOn')
+    const darkmodeActivated = window.localStorage.getItem("dark-mode_toggle") === "true";
+    const darkmodeNotActivated =window.localStorage.reomveItem("dark-mode_toggle") === "false";
+    useEffects(() => {
+    if(darkmodeActivated === "true"){
+        document.body.classList.add(dark-mode_toggle);
+    }
+    else if(darkmodeNotActivated != "true"){
+            document.body.classList.remove(dark-mode_toggle);
+        }
+    });
+}
+return [storedValue, setStoredValue];
+
+export default useDarkMode;
