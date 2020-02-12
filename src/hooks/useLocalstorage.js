57d8537469505ab876dfd670ea6ff59dd5957ebb
Kevin <knphillips01@gmail.com>
2020-02-12 16:54:17 -0600

    updated parameters

--- src/hooks/useLocalstorage.js
+++ src/hooks/useLocalstorage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const useLocalstorage = () => {
+export const useLocalstorage = (key, initialValue) => {
 
  const [storedValue, setStoredValue] = useState(() => {
     const item = window.localStorage.getItem(key);
