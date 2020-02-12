9432d8e51d1c794256661add0963cbd8e46a95d5
Mandi Haase <haase1020@gmail.com>
2020-02-12 16:38:10 -0500

    add useLocalStorage function content

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,5 +2,14 @@ import { useState } from 'react';
 
 export const useLocalStorage =(key, initialState = '') => {
 
-    
+    const [storedValue, setStoredValue] = useState(() => {
+        if (JSON.parse(window.localStorage.getItem(key))){
+            return JSON.parse(window.localStorage.getItem(key));
+        }
+        else {
+            window.localStorage.setItem(key, JSON.stringify(initialState));
+            return initialState;
+        }
+    });
+
 }
