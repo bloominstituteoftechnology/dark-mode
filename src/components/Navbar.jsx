21d09b68e3aa03fae8829c08782ce268725a0761
Your name <a.m.verdine@gmail.com>
2020-02-19 06:57:28 -0500

    Build dropdown into navbar component and wire up clickHandler

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { useDarkMode } from "../hooks/useDarkMode";
 
 const Navbar = () => {
@@ -7,16 +8,33 @@ const Navbar = () => {
     e.preventDefault();
     setEnable(!enable);
   };
+
+  const karatHandler = () => {
+    console.log("clicked Me!");
+  };
+
   return (
-    <nav className="navbar">
-      <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={toggleMode}
-          className={enable ? "toggle toggled" : "toggle"}
-        />
-      </div>
-    </nav>
+    <Router>
+      <nav className="navbar">
+        <h1>Crypto Tracker</h1>
+        <div className="coin-option">
+          <label htmlFor="coins">Select Currency:</label>
+
+          <select id="coins">
+            <option value="placeholder">Select...</option>
+            <option value="Karatgold Coin" onClick={karatHandler}>
+              Karatgold Coin
+            </option>
+          </select>
+        </div>
+        <div className="dark-mode__toggle">
+          <div
+            onClick={toggleMode}
+            className={enable ? "toggle toggled" : "toggle"}
+          />
+        </div>
+      </nav>
+    </Router>
   );
 };
 
