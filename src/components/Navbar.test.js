0be4885f21402272c2b78ca02c179fc405edf230
Matthew Bolton <matthewwbolton@gmail.com>
2020-03-11 16:15:44 -0400

    setup custom hooks for darkMode toggle and one to persist darkMode on/off in localStorage

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as rtl from '@testing-library/react';
 import Navbar from './Navbar';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 test('renders Navbar without crashing', () => {
   rtl.render(<Navbar />);
