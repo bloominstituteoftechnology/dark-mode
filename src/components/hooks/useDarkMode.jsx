cd1db0931aa6974082b39764fb325973674457f1
yirano <yirano@protonmail.ch>
2020-03-24 20:23:24 -0400

    Commit MVP

--- src/components/hooks/useDarkMode.jsx
+++ src/components/hooks/useDarkMode.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocalStorage } from './useLocalStorage'
 
 export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage("dark-mode")
 
+  useEffect(() => {
+    darkMode ? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode');
+  }, [darkMode])
+
+  return [darkMode, setDarkMode]
 }
+
