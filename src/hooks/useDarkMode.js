76386837696fa8f1ec3b537811739beababb48b4
vishalicious213 <vishalicious213@gmail.com>
2020-04-14 22:03:04 -0400

    test output of if/else in useDarkMode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,16 @@
+import { useEffect } from 'react';
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = () => {
-    const [darkModeOn, setDarkModeOn] = useLocalStorage('on');
+    const [darkModeOn, setDarkModeOn] = useLocalStorage(false);
+
+    useEffect(() => {
+        if (darkModeOn === true) {
+            console.log('Dark Mode On')
+        } else
+            {console.log('Dark Mode Off')}
+        // document.title = `Count is: ${count}`;
+    }, [darkModeOn])
 }
+
+export default useDarkMode;
