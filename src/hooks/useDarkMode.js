1692a4444cdd0de636467337731dcec2a7873ad9
Ariel Rodriguez <Ariels713@gmail.com>
2020-04-15 03:55:39 +0000

    finished darkmode hook i think

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,13 @@
+import {useEffect} from 'react'
 import useLocalStorage from './useLocalStorage'
+import { classBody } from '@babel/types'
 
 const useDarkMode = () => {
-    useLocalStorage("true", )
+    const [value, setValue] = useLocalStorage("dark-mode")
+
+    useEffect(() => {
+        useLocalStorage ? body.classList.add('dark-mode') : body.classList.remove('dark-mode')
+    }, [value])
+
+    return [value, setValue]
 }
