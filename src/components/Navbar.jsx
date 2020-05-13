7f1c2bf03c0aa2743be995cd7696d6d58d2772a6
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2020-05-12 22:49:45 -0400

    project re-do

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {useDarkMode} from '../hooks/useDarkMode';
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+const Navbar = (props) => {
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
+    props.setDark(!props.useDark)
   };
+  const [darkMode, setDarkMode] = useDarkMode(toggleMode);
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
--- src/index.js
+++ src/index.js
@@ -2,25 +2,42 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+import {useLocalStorage} from './hooks/useLocalStorage';
+import {useDarkMode} from './hooks/useDarkMode';
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
 const App = () => {
-  const [coinData, setCoinData] = useState([]);
+  const [coinData, setCoinData] = useLocalStorage('data', 'bob');
+  const [useDark, setDark] = useDarkMode();
+  const [darkClass, setDarkClass] = useState('none');
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        console.log(res.data);
+        setCoinData(res.data);
+      })
       .catch(err => console.log(err));
   }, []);
+
+  useEffect(() => {
+    if(useDark){
+      setDarkClass('dark-mode');
+      console.log('Dark Class is working');
+    } else {
+      setDarkClass('none')
+    }
+  }, [useDark]);
+
   return (
-    <div className="App">
-      <Navbar />
+    <div className={darkClass}>
+      <Navbar useDark={useDark} setDark={setDark}/>
       <Charts coinData={coinData} />
     </div>
   );
