fcf1c940ec77acb934dbb6b4cec73c3f89004e7a
Dessa Goodlett <53787065+dmhabh1992@users.noreply.github.com>
2020-05-16 12:32:36 -0500

    updated content

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,10 +4,11 @@ import React, { useState } from "react";
 function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     if (window.localStorage.getItem(key)) {
-     JSON.parse(window.localStorage.getItem(key))}
-     window.localStorage.setItem(key, JSON.stringify(initialValue))
-     return initialValue;
-  });
+     JSON.parse(window.localStorage.getItem(key), console.log(key))}
+    // window.localStorage.setItem(key, JSON.stringify(initialValue))
+    else { window.localStorage.setItem(key, JSON.stringify(initialValue))
+      return initialValue;}
+    });
   const setValue = value => {
     setStoredValue(value);
     window.localStorage.setItem(key, JSON.stringify(value));
