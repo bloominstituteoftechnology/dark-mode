8b30508cc4085e7cc77fb64faaae3aa2466062cd
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-08 20:37:43 -0400

    Stretch

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -28,7 +28,7 @@ const Chart = ({ sparklineData }) => {
 
   return (
     <LineChart width={1100} height={300} data={formattedData}>
-      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      <Line type="monotone" dataKey="value" stroke="#ff3300" strokeWidth="3" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="date" interval={3} />
       <YAxis />
