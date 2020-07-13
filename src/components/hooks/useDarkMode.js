426f3dfdfb252aba74272aebb53c63e87ae462d0
JenVest2020 <jenvest2019@gmail.com>
2020-07-13 17:33:13 -0500

    working on useEffect in DM hook

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+export const useDarkMode = () => {
+    const [someValue, setSomeValue] = useLocalStorage(initialValue);
+
+    // useEffect(() => {
+    //     window.localStorage.value === true ? return( <body className='dark-mode' : ''>)
+    // },[]);
+
+    return [];
+}
