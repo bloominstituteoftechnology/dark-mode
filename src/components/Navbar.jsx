872dfcc46bc40fb3cff0ef0bad4b469e6b0fc668
Jonathan Daly <dalyj138@gmail.com>
2020-07-29 20:21:28 -0400

    moved setValue function outsidethe useState hook and added it to the returned array.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDarkMode } from "../hooks/useDarkMode";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = (e) => {
     e.preventDefault();
-    // setDarkMode(!darkMode);
     setDarkMode(!darkMode);
   };
   return (
