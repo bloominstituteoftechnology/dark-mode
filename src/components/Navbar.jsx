75ab7b211784bdb1533edcb39370a2c3c78a9b12
kato hawkins <kristhahawkins@icloud.com>
2020-09-10 00:57:47 -0400

    mvp finally done :0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
+import { useDarkMode } from "../hooks/useDarkMode"
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  const [darkMode, toggleMode] = useDarkMode(false);
+  
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
