cc1bd6c247ad3896da4b766f5921b699c2bd2c6e
Dustin Myers <dustinmyers@users.noreply.github.com>
2020-09-28 10:36:38 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 
-const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+const Navbar = (props) => {
   const toggleMode = e => {
     e.preventDefault();
-    setDarkMode(!darkMode);
+    props.setDarkMode(!props.darkMode);
   };
   return (
     <nav className="navbar">
@@ -12,7 +11,7 @@ const Navbar = () => {
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={props.darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
