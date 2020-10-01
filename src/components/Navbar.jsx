85098ac7a986d4964418b1dc2bd50cbd8d21928b
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-09-30 20:58:50 -0400

    MVP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import useDarkMode from '../hooks/useDarkMode'
 
 const Navbar = (props) => {
+
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
   const toggleMode = e => {
-    e.preventDefault();
-    props.setDarkMode(!props.darkMode);
+    e.preventDefault()
+    props.setDarkMode(!props.darkMode)
   };
   return (
     <nav className="navbar">
@@ -18,4 +22,4 @@ const Navbar = (props) => {
   );
 };
 
-export default Navbar;
+export default Navbar
