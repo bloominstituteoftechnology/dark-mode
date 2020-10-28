e1a525a56245fed04b042118d51e0d4a26f671e7
unknown <xiongjoh@gmail.com>
2020-10-28 14:48:41 -0500

    mvp finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+
 const Navbar = (props) => {
   const toggleMode = e => {
     e.preventDefault();
--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
