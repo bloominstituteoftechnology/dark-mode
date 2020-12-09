a28989ee633733ea6d5552e72216751ddb5a0273
bria barry <briabarry@iMac.local>
2020-12-09 11:23:13 -0800

    MVP met

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useDarkMode from '../hooks/useDarkMode';
 
-const Navbar = (props) => {
+const Navbar = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false)
   const toggleMode = e => {
     e.preventDefault();
-    props.setDarkMode(!props.darkMode);
+    setDarkMode(!darkMode);
   };
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
+      <div onClick={toggleMode} className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
-          className={props.darkMode ? 'toggle toggled' : 'toggle'}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import useLocalStorage from './useLocalStorage'
+import { useEffect } from 'react';
+
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+
+    useEffect(() => {
+        darkMode 
+        ? document.querySelector('body').classList.add('dark-mode') 
+        : document.querySelector('body').classList.remove('dark-mode')
+    }, [darkMode])
+
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
