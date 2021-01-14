f2e6cfb8c6d70c1c038430ab1e9ec6081343855e
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2021-01-14 07:04:13 -0500

    fininshed final

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import useLocalStorage from "./useLocalStorage"
+
+const useDarkMode = (darkMode) => {
+  const [name, setName] = useLocalStorage("name", darkMode);
+  console.log(name)
+  return [name, setName]
+
+}
+
+export default useDarkMode;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import useDarkMode from "./hooks/useDarkMode"
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
@@ -9,7 +10,7 @@ import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
