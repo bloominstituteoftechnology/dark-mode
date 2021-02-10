87f6fce40d34a4a166752dfafb59104e98bb0d34
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-02-10 18:45:58 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -7,17 +7,19 @@ import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
+import { useDarkMode } from "./hooks/useDarkMode";
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+      .then((res) => setCoinData(res.data))
+      .catch((err) => console.log(err));
   }, []);
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
