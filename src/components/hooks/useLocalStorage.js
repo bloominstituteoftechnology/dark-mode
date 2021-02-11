1cc7f45a76f5d26b1410c4db918a56e1a6a8d89e
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-11 05:46:02 -0500

    commit

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -9,6 +9,7 @@ const [storedValue,setStoredValue] = useState(()=>{
     return item ? JSON.parse(item) : initialValue;
 })
     const setValue = value =>{
+        setStoredValue(value)
         window.localStorage.setItem(key, JSON.stringify(value))
     }
 
