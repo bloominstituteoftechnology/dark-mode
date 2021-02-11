441ed4027f25f4e04ed391849689db1dd8ece14f
hdm007 <harry.mardirossian13@gmail.com>
2021-02-11 00:23:01 -0500

    dark mode composed

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import {useState, useEffect} from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValues) => {
+    const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValues);
+
+    return [darkMode, setDarkMode];
+}
+
+export default useDarkMode;
