9331e384d9a7693e41b6340d3f082873d1bf2115
Sam Lalli <2sam.lalli@gmail.com>
2021-04-09 12:12:15 -0600

    bug fixes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,10 @@ const App = () => {
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        setCoinData(res.data)
+        console.log(res.data)
+      })
       .catch(err => console.log(err));
   }, []);
   return (
